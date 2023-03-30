dd1747bf2a95c4131e613871946a74daaf9a33b3
Emmanuel BIGEON <43699856+EmmanuelBIGEON@users.noreply.github.com>
2023-03-30 11:02:34 +0200

    Update arch_support.cpp

--- src/arch_support.cpp
+++ src/arch_support.cpp
@@ -1,43 +1,18 @@
 #include "arch_support.h"
 
-#include <stdio.h>
-#include <immintrin.h>
 #include <iostream>
-
-
 
 void DisplayArchSupport()
 {
-
-    std::cout << "AVX is supported: " << g_bIsAVXSupported << std::endl;
-    std::cout << "AVX2 is supported: " << g_bIsAVX2Supported << std::endl;
-    std::cout << "AVX512 is supported: " << g_bIsAVX512Supported << std::endl;
-    std::cout << "SSSE3 is supported: " << g_bIsSSSE3Supported << std::endl;
-    std::cout << "SSSE4 is supported: " << g_bIsSSSE4Supported << std::endl;
-
+    std::cout << "AVX support: " << g_bIsAVXSupported << std::endl;
+    std::cout << "AVX2 support: " << g_bIsAVX2Supported << std::endl;
+    std::cout << "AVX512 support: " << g_bIsAVX512Supported << std::endl;
+    std::cout << "SSSE3 support: " << g_bIsSSSE3Supported << std::endl;
+    std::cout << "SSSE4 support: " << g_bIsSSSE4Supported << std::endl;
 }
 
-bool IsAVXSupported()
-{
-    return g_bIsAVXSupported;
-}
-
-bool IsAVX2Supported()
-{
-    return g_bIsAVX2Supported;
-}
-
-bool IsAVX512Supported()
-{
-    return g_bIsAVX512Supported;
-}
-
-bool IsSSSE3Supported()
-{
-    return g_bIsSSSE3Supported;
-}   
-
-bool IsSSSE4Supported()
-{
-    return g_bIsSSSE4Supported;
-}
+bool IsAVXSupported(){return g_bIsAVXSupported;}
+bool IsAVX2Supported(){return g_bIsAVX2Supported;}
+bool IsAVX512Supported(){return g_bIsAVX512Supported;}
+bool IsSSSE3Supported(){return g_bIsSSSE3Supported;}   
+bool IsSSSE4Supported(){return g_bIsSSSE4Supported;}
